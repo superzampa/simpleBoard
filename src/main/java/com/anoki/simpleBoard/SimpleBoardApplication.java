6ec1c1fb510fb8259ac32bf7979ad50ded520dbe
superzampa <superzampa@gmail.com>
2017-11-09 00:40:39 +0100

    first commit

---
+++ src/main/java/com/anoki/simpleBoard/SimpleBoardApplication.java
@@ -0,0 +1,12 @@
+package com.anoki.simpleBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBoardApplication.class, args);
+	}
+}
