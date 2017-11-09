9256665ec597e745161ca981b7f4354b6d8fcb92
superzampa <superzampa@gmail.com>
2017-11-09 16:37:09 +0100

    added security and bootstrap

---
+++ src/main/java/com/anoki/simpleBoard/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.anoki.simpleBoard.config;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	DataSource dataSource;
+	
+	@Autowired
+	private AccessDeniedHandler accessDeniedHandler;
+
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+
+	  auth.jdbcAuthentication().dataSource(dataSource)
+		.usersByUsernameQuery(
+			"select username,password, 1 from USERS where username=?")
+		.authoritiesByUsernameQuery(
+			"select username, user_type from USERS where username=?");
+	}
+
+	/*@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+	  http.authorizeRequests()
+		.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+		.and()
+		  .formLogin().loginPage("/login").failureUrl("/login?error")
+		  .usernameParameter("username").passwordParameter("password")
+		.and()
+		  .logout().logoutSuccessUrl("/login?logout")
+		.and()
+		  .exceptionHandling().accessDeniedPage("/403")
+		.and()
+		  .csrf();
+	}*/
+   @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests()
+					.antMatchers("/", "/home", "/about").permitAll()
+					.antMatchers("/admin/**").hasAnyRole("ADMIN")
+					.antMatchers("/user/**").hasAnyRole("USER")
+					.anyRequest().authenticated()
+                .and()
+                .formLogin()
+					.loginPage("/login")
+					.permitAll()
+					.and()
+                .logout()
+					.permitAll()
+					.and()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+    }
+
+}
