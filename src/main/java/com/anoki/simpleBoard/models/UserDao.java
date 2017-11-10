07c6158762f4a56e3895398add1ce0c8021017ff
superzampa <superzampa@gmail.com>
2017-11-10 01:12:25 +0100

    added user classes

---
+++ src/main/java/com/anoki/simpleBoard/models/UserDao.java
@@ -0,0 +1,123 @@
+package com.anoki.simpleBoard.models;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * This class is used to access data for the User entity.
+ * Repository annotation allows the component scanning support to find and 
+ * configure the DAO wihtout any XML configuration and also provide the Spring 
+ * exceptiom translation.
+ * Since we've setup setPackagesToScan and transaction manager on
+ * DatabaseConfig, any bean method annotated with Transactional will cause
+ * Spring to magically call begin() and commit() at the start/end of the
+ * method. If exception occurs it will also call rollback().
+ */
+@Repository
+@Transactional
+public class UserDao {
+  
+  // ------------------------
+  // PUBLIC METHODS
+  // ------------------------
+  
+  /**
+   * Save the user in the database.
+   */
+  public void create(User user) {
+    entityManager.persist(user);
+    return;
+  }
+  
+  /**
+   * Delete the user from the database.
+   */
+  public void delete(User user) {
+    if (entityManager.contains(user))
+      entityManager.remove(user);
+    else
+      entityManager.remove(entityManager.merge(user));
+    return;
+  }
+  
+  /**
+   * Return all the users stored in the database.
+   */
+  @SuppressWarnings("unchecked")
+  public List<User> getAll() {
+    return entityManager.createQuery("from User").getResultList();
+  }
+  
+  /**
+   * Return the user having the passed email.
+   */
+  public User getByEmail(String email) {
+    return (User) entityManager.createQuery(
+        "from User where email = :email")
+        .setParameter("email", email)
+        .getSingleResult();
+  }
+
+  /**
+   * Return the user having the passed id.
+   */
+  public User getById(long id) {
+    return entityManager.find(User.class, id);
+  }
+
+  /**
+   * Update the passed user in the database.
+   */
+  public void update(User user) {
+    entityManager.merge(user);
+    return;
+  }
+
+  // ------------------------
+  // PRIVATE FIELDS
+  // ------------------------
+  
+  // An EntityManager will be automatically injected from entityManagerFactory
+  // setup on DatabaseConfig class.
+  @PersistenceContext
+  private EntityManager entityManager;
+  
+} // class UserDao
+
+
+
+/*
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * A DAO for the entity User is simply created by extending the CrudRepository
+ * interface provided by spring. The following methods are some of the ones
+ * available from such interface: save, delete, deleteAll, findOne and findAll.
+ * The magic is that such methods must not be implemented, and moreover it is
+ * possible create new query methods working only by defining their signature!
+ * 
+ */
+
+/*
+@Transactional
+public interface UserDao extends CrudRepository<User, Long> {
+
+  /**
+   * Return the user having the passed email or null if no user is found.
+   * 
+   * @param email the user email.
+   */
+  /*
+  public User findByEmail(String email);
+
+} // class UserDao
+
+*/
